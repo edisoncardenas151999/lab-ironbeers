944b56e7dde74a82fdadbab9b28d8e368ecc5057
edison cardenas <edisoncardenas151999@yahooo.com>
2022-09-09 19:14:38 -0400

    done

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + "/views/partials")
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +19,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers() // .getBeers() is the method provided by punkAPI
+    .then((responseFromDB) => {
+      // console.log("Response is:",  responseFromDB);
+      // beers is the hbs file that's gonna be rendered, it comes from "views" folder
+      //  ^
+      //  |-------------- |          "beers" is the name of a variable we will use in hbs file
+      //                  |             |
+      res.render('beers/beers.hbs', { beers: responseFromDB });
+    })
+    .catch((error) => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+  })
+  .catch(error => console.log(error));
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
